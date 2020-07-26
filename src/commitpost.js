d229063b442087b4a944e047c4eb60d3015bdd89
Michael Standen <screaminghawk@gmail.com>
2020-07-27 10:47:11 +1200

    Add post to index

--- src/commitpost.js
+++ src/commitpost.js
@@ -1,7 +1,9 @@
 const git = require('simple-git')()
+const { indexPath } = require('./writepost')
 
 module.exports.commitEntry = async entry => {
 	await git.add('./posts/*')
+	await git.add(indexPath())
 	await git.commit(`Add ${entry.createdDate}`)
 	await git.push()
 }
--- src/writepost.js
+++ src/writepost.js
@@ -2,8 +2,19 @@ const fs = require('fs')
 const path = require('path');
 
 module.exports.filePath = entry => path.join(__dirname, '../posts', `${entry.createdDate}.json`)
+module.exports.indexPath = () => path.join(__dirname, '../index.html')
 
 module.exports.writeEntry = entry => {
+	// Write JSON
 	const fname = module.exports.filePath(entry)
 	fs.writeFileSync(fname, JSON.stringify(entry))
+
+	// Write to index.html
+	const indexPath = module.exports.indexPath()
+	let index = fs.readFileSync(indexPath, {encoding:'utf8', flag:'r'})
+	if (!index.match(new RegExp(`/${entry.createdDate}/`))){
+		// New entry
+		index += `\t\t\t<section><h2>${entry.content}</h2><span>${entry.createdDate} ${entry.createdTime}</span></section>\r\n`
+		fs.writeFileSync(indexPath, index)
+	}
 }
