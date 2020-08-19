4766fc95854e003e7ae87d6d3467675fc540c971
Michael Standen <screaminghawk@gmail.com>
2020-08-20 08:10:34 +1200

    Update git logging

--- src/git.js
+++ src/git.js
@@ -2,12 +2,13 @@ const git = require('simple-git')()
 const { indexPath } = require('./file')
 
 module.exports.commitEntry = async entry => {
-	console.debug("Pushing it up")
+	console.debug("Committing entry")
 	await git.add('./posts/*')
 	await git.add(indexPath())
 	await git.commit(`${entry.updated ? 'Updated' : 'Added'} ${entry.createdDate}`)
 }
 
 module.exports.push = async () => {
+	console.debug("Pushing it up")
 	await git.push()
 }
