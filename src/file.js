e16501e91b4dfcf221f66d82fd3ffe2b07947c77
Michael Standen <screaminghawk@gmail.com>
2020-08-20 09:06:02 +1200

    Styles are nice in mobile too #2

--- src/file.js
+++ src/file.js
@@ -55,7 +55,7 @@ module.exports.writeEntry = entry => {
 		split.pop()
 		index = split.join('\t\t\t')
 	}
-	index += `\t\t\t<section class="font${randLimit(3) + 1}" color="hsla(${randLimit(360)}, 70%, 70%, 0.8)"><h2>${entry.content.replace(/\n/g, "<br/>")}</h2><span>${entry.createdDate} ${entry.createdTime}</span></section>\r\n`
+	index += `\t\t\t<section class="font${randLimit(3) + 1}" style="background-color: hsla(${randLimit(360)}, 70%, 70%, 0.4)"><h2>${entry.content.replace(/\n/g, "<br/>")}</h2><span>${entry.createdDate} ${entry.createdTime}</span></section>\r\n`
 	fs.writeFileSync(indexPath, index)
 
 	return true
